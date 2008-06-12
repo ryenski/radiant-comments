7767c6b8fc3bae9c33e7a4b712a175570c528717
Ryan Heneise <ryan@artofmission.com>
2008-06-12 08:38:28 -0500

    Added more detail to the comment notification email

--- app/models/comment_mailer.rb
+++ app/models/comment_mailer.rb
@@ -1,9 +1,9 @@
 class CommentMailer < ActionMailer::Base
   
-  def comment_notification(url, sent_at = Time.now)
+  def comment_notification(comment, sent_at = Time.now)
     site_name   = Radiant::Config['comments.notification_site_name']
     @subject    = "New comment posted requires approval"
-    @body       = {@name = site_name, @url = url}
+    @body       = {@name = site_name, @comment = comment}
     @recipients = Radiant::Config['comments.notification_to']
     @from       = Radiant::Config['comments.notification_from']
     @sent_on    = sent_at
